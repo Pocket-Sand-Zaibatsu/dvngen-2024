b8f71fad75b957a4d8b0766e6cdf3b6229e87a18
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-03 14:04:46 -0600

    Draft Character scene

---
+++ scenes/character/character.gd
@@ -0,0 +1,24 @@
+extends Area2D
+class_name Character
+
+@export var animation_speed := 2
+@export var tile_size = 16
+var moving = false
+var inputs = {
+	"right": Vector2.RIGHT,
+	"left": Vector2.LEFT,
+	"up": Vector2.UP,
+	"down": Vector2.DOWN
+}
+
+@onready var ray = $RayCast2d
+
+func move(dir):
+	ray.target_position = inputs[dir] * tile_size
+	ray.force_raycast_update()
+	if !ray.is_colliding():
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "position", position + inputs[dir] * tile_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
+		moving = true
+		await tween.finished
+		moving = false
