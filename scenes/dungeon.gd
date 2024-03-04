6e50c963e14c64eef1a3b3f74d8e746d6558c9bd
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-04 12:44:55 -0600

    Create dungeon scene

---
+++ scenes/dungeon.gd
@@ -0,0 +1,16 @@
+extends Node
+class_name Dungeon
+var level_generator_scene = preload("res://scenes/levels/level_generator.tscn")
+var player_scene = preload("res://scenes/character/player/player.tscn")
+
+@onready var player: Player
+@onready var level_generator: LevelGenerator
+@onready var hud: Hud = get_node("Hud")
+@onready var dungeon_viewport = get_node("DungeonViewportContainer/DungeonViewport")
+
+func _ready() -> void:
+	player = player_scene.instantiate()
+	player.name = "Player"
+	add_child(player)
+	level_generator = level_generator_scene.instantiate()
+	add_child(level_generator)
