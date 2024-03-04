41498f14d9e5167b98b1a129853cafe2ff5b0206
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-03 19:48:26 -0600

    Simplify how directions are used

--- scenes/character/character.gd
+++ scenes/character/character.gd
@@ -4,11 +4,17 @@ class_name Character
 @export var animation_speed := 10
 @export var tile_size = 16
 var moving = false
-var inputs = {
-	"ui_right": Vector2.RIGHT,
-	"ui_left": Vector2.LEFT,
-	"ui_up": Vector2.UP,
-	"ui_down": Vector2.DOWN
+var direction_vector = {
+	"Right": Vector2.RIGHT,
+	"Left": Vector2.LEFT,
+	"Up": Vector2.UP,
+	"Down": Vector2.DOWN
+}
+var input_to_direction = {
+	"ui_right": "Right",
+	"ui_left": "Left",
+	"ui_up": "Up",
+	"ui_down": "Down"
 }
 
 @onready var ray = get_node("RayCast2D")
@@ -22,12 +28,13 @@ func spawn(spawn_position: Vector2i) -> void:
 	position = spawn_position * tile_size
 	position += Vector2.ONE * tile_size / 2
 
-func move(dir):
-	ray.target_position = inputs[dir] * tile_size
+func move(direction: String):
+	ray.target_position = direction_vector[direction] * tile_size
 	ray.force_raycast_update()
+	animated_sprite.play(direction)
 	if !ray.is_colliding():
 		var tween = get_tree().create_tween()
-		tween.tween_property(self, "position", position + inputs[dir] * tile_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
+		tween.tween_property(self, "position", position + direction_vector[direction] * tile_size, 1.0 / animation_speed).set_trans(Tween.TRANS_SINE)
 		moving = true
 		await tween.finished
 		moving = false
