a4c7b894c3db0adbda4ffd1a8ad564a77e45b71c
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-04 14:42:42 -0600

    Pass player position from parent

--- scenes/dungeon.gd
+++ scenes/dungeon.gd
@@ -7,6 +7,7 @@ var hud_scene = preload("res://interfaces/hud/hud.tscn")
 
 signal dev_tools_stat_update(stat_field: String, value: int)
 signal player_ready
+signal player_position_updated(position: Vector2i)
 
 @onready var player: Player
 @onready var level_generator: LevelGenerator
@@ -16,6 +17,7 @@ func _ready() -> void:
 	player = player_scene.instantiate()
 	player.name = "Player"
 	add_child(player)
+	player.player_position_updated.connect(_pass_player_position_updated)
 	level_generator = level_generator_scene.instantiate()
 	level_generator.name = "LevelGenerator"
 	add_child(level_generator)
@@ -27,3 +29,7 @@ func _ready() -> void:
 func _pass_dev_tools_stat_updates(stat_field: String, value: int) -> void:
 	print("Dungeon dev tools: ", stat_field, ":", value)
 	dev_tools_stat_update.emit(stat_field, value)
+
+func _pass_player_position_updated(position: Vector2i) -> void:
+	print(position)
+	player_position_updated.emit(position)
