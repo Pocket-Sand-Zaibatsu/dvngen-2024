cdc007fe110cf08444bb5c48d8913ea2047763f3
CJ Harries <thecjharries@users.noreply.github.com>
2024-03-03 19:51:35 -0600

    Feat/tidy player (#8)
    * Add resources to override
    
    * Initialize character properly
    
    * Simplify var name for Character
    
    * Organize children to match Character
    
    * Simplify how directions are used
    
    * Simplify player movement

--- scenes/character/character.gd
+++ scenes/character/character.gd
@@ -4,40 +4,37 @@ class_name Character
 @export var animation_speed := 10
 @export var tile_size = 16
 var moving = false
-var inputs = {
-	"ui_right": Vector2.RIGHT,
-	"ui_left": Vector2.LEFT,
-	"ui_up": Vector2.UP,
-	"ui_down": Vector2.DOWN
+var direction_vector = {
+	"Right": Vector2.RIGHT,
+	"Left": Vector2.LEFT,
+	"Up": Vector2.UP,
+	"Down": Vector2.DOWN
+}
+var input_to_direction = {
+	"ui_right": "Right",
+	"ui_left": "Left",
+	"ui_up": "Up",
+	"ui_down": "Down"
 }
 
 @onready var ray = get_node("RayCast2D")
-@onready var _animated_sprite = $AnimatedSprite2D
+@onready var animated_sprite = get_node("AnimatedSprite2D")
+@onready var audio_player = get_node("AudioStreamPlayer2D")
 
-func _ready():
-	position = Vector2i(100, 100)
+func _ready() -> void:
+	animated_sprite.play("Down")
 
 func spawn(spawn_position: Vector2i) -> void:
 	position = spawn_position * tile_size
 	position += Vector2.ONE * tile_size / 2
 
-func _process(_delta):
-	if Input.is_action_pressed("ui_right"):
-		_animated_sprite.play("Right")
-	if Input.is_action_pressed("ui_left"):
-		_animated_sprite.play("Left")
-	if Input.is_action_pressed("ui_up"):
-		_animated_sprite.play("Up")
-	if Input.is_action_pressed("ui_down"):
-		_animated_sprite.play("Down")
-		
-		
-func move(dir):
-	ray.target_position = inputs[dir] * tile_size
+func move(direction: String):
+	ray.target_position = direction_vector[direction] * tile_size
 	ray.force_raycast_update()
+	animated_sprite.play(direction)
 	if !ray.is_colliding():
 		var tween = get_tree().create_tween()
-		tween.tween_property(self, "position", position + inputs[dir] * tile_size, 1.0/animation_speed).set_trans(Tween.TRANS_SINE)
+		tween.tween_property(self, "position", position + direction_vector[direction] * tile_size, 1.0 / animation_speed).set_trans(Tween.TRANS_SINE)
 		moving = true
 		await tween.finished
 		moving = false
