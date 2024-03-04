2b230025f82dc9c8492e570dc28b7784aef9a27b
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-04 12:47:15 -0600

    Remove missing resource

--- scenes/dungeon.gd
+++ scenes/dungeon.gd
@@ -6,7 +6,6 @@ var player_scene = preload("res://scenes/character/player/player.tscn")
 @onready var player: Player
 @onready var level_generator: LevelGenerator
 @onready var hud: Hud = get_node("Hud")
-@onready var dungeon_viewport = get_node("DungeonViewportContainer/DungeonViewport")
 
 func _ready() -> void:
 	player = player_scene.instantiate()
