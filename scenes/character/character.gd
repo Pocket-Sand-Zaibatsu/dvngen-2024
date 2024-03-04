7e8857638cd77695d7a1d147f81abb82606e17d7
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-03 19:41:19 -0600

    Simplify var name for Character

--- scenes/character/character.gd
+++ scenes/character/character.gd
@@ -12,27 +12,16 @@ var inputs = {
 }
 
 @onready var ray = get_node("RayCast2D")
-@onready var _animated_sprite = get_node("AnimatedSprite2D")
+@onready var animated_sprite = get_node("AnimatedSprite2D")
 @onready var audio_player = get_node("AudioStreamPlayer2D")
 
-func _ready():
-	_animated_sprite.play("Down")
+func _ready() -> void:
+	animated_sprite.play("Down")
 
 func spawn(spawn_position: Vector2i) -> void:
 	position = spawn_position * tile_size
 	position += Vector2.ONE * tile_size / 2
 
-func _process(_delta):
-	if Input.is_action_pressed("ui_right"):
-		_animated_sprite.play("Right")
-	if Input.is_action_pressed("ui_left"):
-		_animated_sprite.play("Left")
-	if Input.is_action_pressed("ui_up"):
-		_animated_sprite.play("Up")
-	if Input.is_action_pressed("ui_down"):
-		_animated_sprite.play("Down")
-		
-		
 func move(dir):
 	ray.target_position = inputs[dir] * tile_size
 	ray.force_raycast_update()
