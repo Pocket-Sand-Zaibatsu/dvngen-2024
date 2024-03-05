bf9f10d52dbb3b8dc2b96d7b509685761152970a
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-05 10:44:04 -0600

    Create script for Bones

---
+++ scenes/world-object/bones/bones.gd
@@ -0,0 +1,12 @@
+@icon("res://assets/sprites/world/td_world_bones_a.png")
+extends Area2D
+class_name Bones
+
+var sprites: Array[String] = [
+	"res://assets/sprites/world/td_world_bones_a.png",
+	"res://assets/sprites/world/td_world_bones_b.png",
+	"res://assets/sprites/world/td_world_bones_d.png",
+]
+
+func _ready():
+	pass
