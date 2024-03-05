8898ea9209796ffbefbfb744368967c6565982d9
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-04 21:42:22 -0600

    Update minotaur to use a* pathing

--- scenes/character/monsters/minotaur/minotaur.gd
+++ scenes/character/monsters/minotaur/minotaur.gd
@@ -2,7 +2,11 @@
 extends Monster
 class_name Minotaur
 
+func spawn(spawn_grid: Vector2i) -> void:
+	super(spawn_grid)
+	print("Minotaur spawning at ", spawn_grid)
+
 func handle_movement(event) -> void:
 	for direction in input_to_direction.keys():
 		if event.is_action_pressed(direction):
-			move("Down")
+			move(LevelGrid.a_star_to_player(position))
