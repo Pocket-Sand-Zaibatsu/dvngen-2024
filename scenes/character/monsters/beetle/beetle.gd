7fccc016125bebf4386ad8ecb731a89cfee9b1a5
RoyceGuillory <RoyceGuillory@gmail.com>
2024-03-05 13:16:00 -0600

    Updated Collision
    Added Collision on the remaining physical objects that will take up space in the tilemap

--- scenes/character/monsters/beetle/beetle.gd
+++ scenes/character/monsters/beetle/beetle.gd
@@ -1,3 +1,31 @@
 @icon("res://assets/sprites/monsters/beetle/td_monsters_beetle_d1.png")
 extends Monster
 class_name Beetle
+
+
+var hp = 13
+var def = 13
+var hit = 3
+var xp = 25
+
+
+var dmg_roll = RandomNumberGenerator.new()
+var atk_roll = RandomNumberGenerator.new()
+
+
+func _ready():
+	var attack = atk_roll.randf_range(1,20) + hit
+	var damage = dmg_roll.randf_range(1,4) + 1
+	if attack >= Player.ac:
+		Player.current_health = Player.current_health - damage
+	
+
+	
+func handle_movement(event) -> void:
+	for direction in input_to_direction.keys():
+		if event.is_action_pressed(direction):
+			move(LevelGrid.a_star_to_player(position))
+
+	
+	
+
