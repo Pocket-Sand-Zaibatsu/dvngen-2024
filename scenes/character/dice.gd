2754fdef03cefb25f97108bb4043e55641a2cc40
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-05 14:31:02 -0600

    Fix results

--- scenes/character/dice.gd
+++ scenes/character/dice.gd
@@ -5,10 +5,11 @@ class_name Dice
 
 @onready var rng: RandomNumberGenerator
 
-func _init(sides: int) -> void:
-	if sides < 1:
+func _init(new_sides: int) -> void:
+	if new_sides < 1:
 		sides = 1
-	self.sides = sides
+	else:
+		sides = new_sides
 	rng = RandomNumberGenerator.new()
 	rng.randomize()
 
@@ -19,7 +20,7 @@ func roll() -> int:
 	return rng.randi_range(1, sides)
 
 func roll_count(count: int) -> Array[int]:
-	var results = []
+	var results: Array[int] = []
 	for _index in range(count):
 		results.push_back(roll())
 	return results
@@ -30,8 +31,9 @@ func sum(accumulator: int, number: int) -> int:
 func roll_count_sum(count: int) -> int:
 	return roll_count(count).reduce(sum, 0)
 
-func roll_count_sum_drop_lowest_x(count: int, drop_count: int = 1) -> int:
+func roll_count_sum_drop_lowest(count: int, drop_count: int = 1) -> int:
 	if drop_count >= count:
 		return 0
 	var rolls = roll_count(count)
-	return rolls.slice(0, count - drop_count - 1).reduce(sum, 0)
+	rolls.sort()
+	return rolls.slice(drop_count, count).reduce(sum, 0)
