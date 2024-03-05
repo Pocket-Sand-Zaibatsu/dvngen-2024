3aa2d2a22b36f83b265b58943f50cc5f979dbc6c
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-05 10:43:15 -0600

    Flesh out world object

--- scenes/world-object/world_object.gd
+++ scenes/world-object/world_object.gd
@@ -3,6 +3,14 @@ class_name WorldObject
 
 @export var tile_size = 16
 
+@onready var rng: RandomNumberGenerator
+@onready var collision_shape = get_node("CollisionShape2D")
+@onready var sprite = get_node("Sprite2D")
+
+func _ready() -> void:
+	rng = RandomNumberGenerator.new()
+	rng.randomize()
+
 func spawn(spawn_grid: Vector2i) -> void:
 	position = LevelGrid.grid_to_position(spawn_grid)
 
