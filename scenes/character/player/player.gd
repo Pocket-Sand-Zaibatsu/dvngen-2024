041c363a783e8aa0f124f061bb37021ba6daf588
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-04 19:08:14 -0600

    Update Player scene for singleton

--- scenes/character/player/player.gd
+++ scenes/character/player/player.gd
@@ -1,14 +1,14 @@
 extends Character
-class_name Player
+class_name PlayerCharacter
 
 signal player_ready
 signal player_position_updated(position: Vector2i)
 
 func _ready() -> void:
-	get_parent().dev_tools_stat_update.connect(_process_dev_tools)
+	#visible = false
 	player_ready.emit()
 
-func _process_dev_tools(stat_field: String, value: int) -> void:
+func _on_dev_tools_stat_update(stat_field: String, value: int) -> void:
 	print("Player dev tools: ", stat_field, ":", value)
 	match stat_field:
 		"max_health":
@@ -19,6 +19,8 @@ func _process_dev_tools(stat_field: String, value: int) -> void:
 			health_changed.emit()
 
 func _unhandled_input(event: InputEvent):
+	if not visible:
+		return
 	for direction in input_to_direction.keys():
 		if event.is_action_pressed(direction):
 			move(input_to_direction[direction])
