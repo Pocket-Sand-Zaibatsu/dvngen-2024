38fc17e0492b9a849d483d4972c0e3680579c26b
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-06 11:48:09 -0600

    Check if the player opens the door

--- scenes/world-object/doors/doors_ew.gd
+++ scenes/world-object/doors/doors_ew.gd
@@ -2,8 +2,12 @@
 extends WorldObject
 class_name EwDoor
 
-func _on_body_entered(_body: Variant):
+signal player_entered
+
+func _on_body_entered(body: Variant):
 	sprite.texture = load("res://assets/sprites/world/td_world_door_stone_h_open.png")
+	if is_body_player(body):
+		player_entered.emit()
 
 func _on_body_exited(_body: Variant):
 	sprite.texture = load("res://assets/sprites/world/td_world_door_stone_h_closed.png")
