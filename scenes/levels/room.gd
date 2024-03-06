935457387ff0ff1cf73f05dd9bd96ed3e27b9b70
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-06 10:36:41 -0600

    Create a class to hold room setup

---
+++ scenes/levels/room.gd
@@ -0,0 +1,52 @@
+extends Node
+class_name Room
+
+const uuid_util = preload("res://addons/uuid/uuid.gd")
+
+signal floor_checked(target_grid: Vector2i)
+
+var boundary: Rect2i
+var uuid: String
+var doors: Array[Vector2i] = []
+var longest_continuous_door_length: int = 0
+
+func _init(new_boundary: Rect2i) -> void:
+	boundary = new_boundary
+	uuid = uuid_util.v4()
+
+func get_center() -> Vector2i:
+	return (boundary.position + boundary.end) / 2
+
+func check_floor(target_grid: Vector2i) -> bool:
+	return "floor" == get_parent().map.get(target_grid, "")
+
+func is_open() -> bool:
+	return 2 < longest_continuous_door_length
+
+func update_doors() -> void:
+	var new_doors: Array[Vector2i] = []
+	var new_longest_continuous_door_length: int = 0
+	var current_continuous_door_length: int = 0
+	for x in [boundary.position.x - 1, boundary.end.x + 1]:
+		for y in range(boundary.position.y, boundary.end.y):
+			if check_floor(Vector2i(x, y)):
+				current_continuous_door_length += 1
+				new_doors.push_back(Vector2i(x, y))
+			else:
+				if current_continuous_door_length > new_longest_continuous_door_length:
+					new_longest_continuous_door_length = current_continuous_door_length
+				current_continuous_door_length = 0
+	for y in [boundary.position.y - 1, boundary.end.y + 1]:
+		for x in range(boundary.position.x, boundary.end.x):
+			if check_floor(Vector2i(x, y)):
+				current_continuous_door_length += 1
+				new_doors.push_back(Vector2i(x, y))
+			else:
+				if current_continuous_door_length > new_longest_continuous_door_length:
+					new_longest_continuous_door_length = current_continuous_door_length
+				current_continuous_door_length = 0
+	doors = new_doors
+	longest_continuous_door_length = new_longest_continuous_door_length
+	print(uuid, " has ", doors)
+	if is_open():
+		print(uuid, " is open")
