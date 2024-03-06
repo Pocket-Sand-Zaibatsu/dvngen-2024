127c19387e261665af89300ac452b3ee1a0eccf4
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-05 18:01:27 -0600

    Ensure trailing newlines are stripped

--- interfaces/hud/game_log_transport.gd
+++ interfaces/hud/game_log_transport.gd
@@ -5,4 +5,6 @@ signal game_log_messaged(contents: String)
 
 func _on_log_messaged(contents: String) -> void:
 	# this is where we'd filter if we want to
+	if contents.ends_with("\n"):
+		contents = contents.trim_suffix("\n")
 	game_log_messaged.emit(contents)
