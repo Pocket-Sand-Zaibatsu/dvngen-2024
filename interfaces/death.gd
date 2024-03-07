9af0ebcfeba1b67463c27f64becf56d493c5cc01
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-07 14:40:19 -0600

    Add basic death stats

--- interfaces/death.gd
+++ interfaces/death.gd
@@ -14,9 +14,13 @@ func _ready():
 		label.set_theme(label_theme)
 		label.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
 		label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
-		label.add_theme_font_size_override("font_size", 32)
+		label.add_theme_font_size_override("font_size", 48)
 		StatsContainer.add_child(label)
 
 
 func _on_main_menu_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/main-menu/main_menu.tscn")
+
+func _on_v_box_container_tree_entered():
+	death_stats.push_back("LEVEL: %d" % Player.level.level)
+	death_stats.push_back("TOTAL XP: %d" % Player.level.current_experience)
