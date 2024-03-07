a53b85b939daaf987fd010bdd44662577b6f9d0c
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-07 12:27:33 -0600

    Remove pregen monster health

--- scenes/character/monsters/monster.gd
+++ scenes/character/monsters/monster.gd
@@ -14,12 +14,6 @@ func _ready():
 	rng.seed = 0
 	damage_sent.connect(Player._on_damage_sent)
 	Player.damage_sent.connect(_on_damage_sent)
-	initialize_health()
-
-func initialize_health() -> void:
-	for _index in range(level):
-		max_health += hit_dice.roll()
-	current_health = max_health
 
 func move(_ui_action: String) -> void:
 	super(LevelGrid.a_star_to_player(position))
