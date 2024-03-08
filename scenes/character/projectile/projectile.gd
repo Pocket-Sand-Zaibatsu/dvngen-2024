5f3cba50d477790c9182a060635b723ccbc72a3b
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-07 22:02:20 -0600

    Have projectile emit damage

--- scenes/character/projectile/projectile.gd
+++ scenes/character/projectile/projectile.gd
@@ -1,11 +1,16 @@
 extends Area2D
 class_name Projectile
 
+signal log_messaged(contents: String)
+
 var uuid: String
 var desired_type: String = "slash"
 var projectile_type: String = "slash"
 var current_travel: int = 1
 var max_range: int = 1
+var actor: String
+var attack_roll: int
+var damage: int
 
 @onready var sprite: AnimatedSprite2D = get_node("AnimatedSprite2D")
 @onready var projectile_types: PackedStringArray = sprite.get_sprite_frames().get_animation_names()
@@ -16,15 +21,16 @@ func _ready():
 		projectile_type = desired_type
 	set_projectile_range()
 	sprite.play(projectile_type)
+	log_messaged.connect(GameLogTransport._on_log_messaged)
 
 func set_projectile_range() -> void:
 	match projectile_type:
-		"arrow":
-			max_range = 20
-		"slash":
+		"slash", "bash":
 			max_range = 1
 		"lightning":
 			max_range = 3
+		"arrow":
+			max_range = 20
 		_:
 			max_range = 10
 
@@ -35,6 +41,11 @@ func set_velocity(new_velocity: Vector2i) -> void:
 	velocity = new_velocity
 	rotation = Vector2(velocity).angle() + PI / 2
 
+func set_attack(new_actor: String, new_attack_roll: int, new_damage: int) -> void:
+	actor = new_actor
+	attack_roll = new_attack_roll
+	damage = new_damage
+
 func spawn(spawn_grid: Vector2i) -> void:
 	position = LevelGrid.grid_to_position(spawn_grid + velocity)
 
@@ -43,8 +54,9 @@ func despawn() -> void:
 	self.queue_free()
 
 func _on_body_entered(body: Variant):
-	body.change_health(-10000)
-	despawn()
+	if body.has_method("_on_damage_sent"):
+		body._on_damage_sent(actor, attack_roll, damage)
+		despawn()
 
 func _on_input_received(_action: String) -> void:
 	current_travel += 1
