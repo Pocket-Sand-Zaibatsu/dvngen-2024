b54b7cbfa655d2671ac58b43c459e2d08fe0731c
CJ Harries <cj@wizardsoftheweb.pro>
2024-03-09 12:35:40 -0600

    Add options back to menu

--- scenes/main-menu/main_menu.gd
+++ scenes/main-menu/main_menu.gd
@@ -1,6 +1,6 @@
 extends CanvasLayer
 
-@onready var class_select_scene = preload("res://scenes/class_select/class_Select.tscn")
+@onready var options_scene: PackedScene = preload("res://interfaces/options/options.tscn")
 
 
 func _ready():
@@ -16,3 +16,8 @@ func _on_start_button_pressed():
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
+func _on_options_button_pressed():
+	var options = options_scene.instantiate()
+	add_child(options)
